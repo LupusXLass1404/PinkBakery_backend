b10b9b1a6a61a1b89bf41e4f24b987f2b7ce5813
LupusXLass1404 <BerkasXLupusXLass@gmail.com>
2025-10-30 22:01:32 +0800

    本地與遠端的env路徑設定

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+// 載入 composer 套件
 require __DIR__ . '/../vendor/autoload.php';
 
 /**
@@ -31,7 +32,21 @@ $app->addRoutingMiddleware();
  * Note: This middleware should be added last. It will not handle any exceptions/errors
  * for middleware added after it.
  */
-$app->setBasePath('/vue/shopping/backend');
+
+
+// 讀取 .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$env = $_ENV['APP_ENV'] ?: 'development';
+
+if ($env === 'development') {
+    $app->setBasePath($_ENV['LOCAL_BASE_PATH']);
+} else {
+    $app->setBasePath($_ENV['PROD_BASE_PATH']);
+}
+
+// $app->setBasePath('/vue/shopping/backend');
 
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
